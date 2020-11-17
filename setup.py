5cf7a565ceca8388f7549adf4a53cb7cd5b302be
Pierre Tholoniat <pierre@tholoniat.com>
2020-11-17 09:11:34 -0500

    Sync cargo and py setup, bumb version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sycret",
-    version="0.1-alpha.1",
+    version="0.1.0",
     author="Pierre Tholoniat",
     author_email="pierre@tholoniat.com",
     packages=find_packages(),
