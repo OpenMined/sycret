c736ecb1564966a708e33fd04202a4db2177f5ba
Pierre Tholoniat <pierre@tholoniat.com>
2020-11-26 17:30:19 -0500

    Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sycret",
-    version="0.1.0",
+    version="0.1.1",
     author="Pierre Tholoniat",
     author_email="pierre@tholoniat.com",
     packages=find_packages(),
