352088e40857282e603b25e516e94ca1e8b795d1
Pierre Tholoniat <pierre@tholoniat.com>
2021-05-09 17:17:08 -0400

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 # third party
 from setuptools import find_packages
 from setuptools import setup
+from pathlib import Path
+
+setup_dir = Path(__file__).resolve().parent
 
 setup(
     name="sycret",
     version="0.1.3",
     author="Pierre Tholoniat",
     author_email="pierre@tholoniat.com",
+    url="https://github.com/OpenMined/sycret",
+    description="Function Secret Sharing library for Python and Rust with hardware acceleration",
+    long_description=Path(setup_dir, "README.md").open().read(),
+    long_description_content_type="text/markdown",
+    license="Apache-2.0",
+    python_requires=">=3.6",
+    install_requires=["numpy"],
     packages=find_packages(),
 )
