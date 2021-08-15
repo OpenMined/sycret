343beb76e794390e7e9ed1e5c6cbe056ee9edac6
Pierre Tholoniat <pierre@tholoniat.com>
2021-08-15 17:44:29 -0400

    Added benchmark for comparison and equality evaluation

--- benches/bench_le.rs
+++ benches/bench_le.rs
@@ -1,31 +1,31 @@
 use rand::Rng;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use sycret::eq::*;
+use sycret::le::*;
 use sycret::stream::{FSSKey, PRG};
 use sycret::utils::MMO;
 
-pub fn eq_keygen(c: &mut Criterion) {
+pub fn le_keygen(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
     let aes_keys: [u128; 4] = rng.gen();
     let mut prg = MMO::from_slice(&aes_keys);
-    c.bench_function("Eq keygen", |b| {
-        b.iter(|| EqKey::generate_keypair(black_box(&mut prg)))
+    c.bench_function("Le keygen", |b| {
+        b.iter(|| LeKey::generate_keypair(black_box(&mut prg)))
     });
 }
 
-pub fn eq_eval(c: &mut Criterion) {
+pub fn le_eval(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
     let aes_keys: [u128; 4] = rng.gen();
     let mut prg = MMO::from_slice(&aes_keys);
 
-    let (k_a, k_b) = EqKey::generate_keypair(&mut prg);
+    let (k_a, k_b) = LeKey::generate_keypair(&mut prg);
     let alpha = k_a.alpha_share.wrapping_add(k_b.alpha_share);
 
-    c.bench_function("Eq eval", |b| {
+    c.bench_function("Le eval", |b| {
         b.iter(|| k_a.eval(black_box(&mut prg), 0, alpha))
     });
 }
 
-criterion_group!(bench_eq, eq_keygen, eq_eval);
-criterion_main!(bench_eq);
+criterion_group!(bench_le, le_keygen, le_eval);
+criterion_main!(bench_le);
