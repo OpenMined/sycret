907c8c66f522734e06adde5a7efb8d3fc02e4e09
Arturo Marquez <arturomf94@gmail.com>
2021-09-01 15:28:18 -0500

    Adjust vector capacity

--- benches/batch.rs
+++ benches/batch.rs
@@ -2,12 +2,12 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use sycret::keygen;
 
 pub fn batch_keygen(c: &mut Criterion) {
-    let mut keys_a: Vec<u8> = Vec::with_capacity(16);
-    let mut keys_b: Vec<u8> = Vec::with_capacity(16);
+    let mut keys_a: Vec<u8> = Vec::with_capacity(3105);
+    let mut keys_b: Vec<u8> = Vec::with_capacity(3105);
     let keys_a_pointer: *mut u8 = keys_a.as_mut_ptr();
     let keys_b_pointer: *mut u8 = keys_b.as_mut_ptr();
-    let n_values: usize = 16;
-    let n_threads: usize = 2;
+    let n_values: usize = 5;
+    let n_threads: usize = 6;
     let op_id: usize = 0;
     unsafe {
         c.bench_function("Batch keygen", |b| {
