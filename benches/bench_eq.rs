90887b6716fbdcbf18ca7a35d2982f84b5d3e8eb
Arturo Marquez <arturomf94@gmail.com>
2021-09-08 23:55:54 -0500

    Add ranges for `n_values` and `n_threads` in Eq

--- benches/bench_eq.rs
+++ benches/bench_eq.rs
@@ -17,19 +17,34 @@ pub fn eq_keygen(c: &mut Criterion) {
 
 /// op_id = 0 implies Eq key generation
 pub fn eq_batch_keygen(c: &mut Criterion) {
-    // 3105 comes from multiplying the Eq key length (621)
-    // by the number of values / n_values (5)
-    let mut keys_a: Vec<u8> = Vec::with_capacity(3105);
-    let mut keys_b: Vec<u8> = Vec::with_capacity(3105);
-    let keys_a_pointer: *mut u8 = keys_a.as_mut_ptr();
-    let keys_b_pointer: *mut u8 = keys_b.as_mut_ptr();
-    let n_values: usize = 5;
-    let n_threads: usize = 6;
-    let op_id: usize = 0;
-    unsafe {
-        c.bench_function("Batch keygen", |b| {
-            b.iter(|| keygen(keys_a_pointer, keys_b_pointer, n_values, n_threads, op_id))
-        });
+    let mut group = c.benchmark_group("Batch Eq keygen");
+    for &n_values in [10, 100, 1000].iter() {
+        let key_length: usize = 621;
+        let mut keys_a: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let mut keys_b: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let keys_a_pointer: *mut u8 = keys_a.as_mut_ptr();
+        let keys_b_pointer: *mut u8 = keys_b.as_mut_ptr();
+        let n_threads: usize = 6;
+        let op_id: usize = 0;
+        unsafe {
+            group.bench_function(format!("n_values: {}", n_values), |b| {
+                b.iter(|| keygen(keys_a_pointer, keys_b_pointer, n_values, n_threads, op_id))
+            });
+        }
+    }
+    for &n_threads in [1, 4, 6].iter() {
+        let key_length: usize = 621;
+        let n_values: usize = 100;
+        let op_id: usize = 0;
+        let mut keys_a: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let mut keys_b: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let keys_a_pointer: *mut u8 = keys_a.as_mut_ptr();
+        let keys_b_pointer: *mut u8 = keys_b.as_mut_ptr();
+        unsafe {
+            group.bench_function(format!("n_threads: {}", n_threads), |b| {
+                b.iter(|| keygen(keys_a_pointer, keys_b_pointer, n_values, n_threads, op_id))
+            });
+        }
     }
 }
 
@@ -47,38 +62,72 @@ pub fn eq_eval(c: &mut Criterion) {
 }
 
 pub fn eq_batch_eval(c: &mut Criterion) {
-    // 3105 comes from multiplying the Eq key length (621)
-    // by the number of values / n_values (5)
-    let mut keys_a: Vec<u8> = Vec::with_capacity(3105);
-    let mut keys_b: Vec<u8> = Vec::with_capacity(3105);
-    let keys_a_pointer: *mut u8 = keys_a.as_mut_ptr();
-    let keys_b_pointer: *mut u8 = keys_b.as_mut_ptr();
-    let n_values: usize = 5;
-    let n_threads: usize = 6;
-    let op_id: usize = 0;
-    unsafe {
-        keygen(keys_a_pointer, keys_b_pointer, n_values, n_threads, op_id);
+    let mut group = c.benchmark_group("Batch Eq eval");
+    for &n_values in [10, 100, 1000].iter() {
+        let key_length: usize = 621;
+        let n_threads: usize = 6;
+        let op_id: usize = 0;
+        let mut keys_a: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let mut keys_b: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let keys_a_pointer: *mut u8 = keys_a.as_mut_ptr();
+        let keys_b_pointer: *mut u8 = keys_b.as_mut_ptr();
+        unsafe {
+            keygen(keys_a_pointer, keys_b_pointer, n_values, n_threads, op_id);
+        }
+        let party_id: usize = 0;
+        let xs: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let xs_pointer: *const u8 = xs.as_ptr();
+        let keys_pointer: *const u8 = keys_a_pointer as *const u8;
+        let mut results: Vec<i64> = Vec::with_capacity(key_length * n_values);
+        let results_pointer: *mut i64 = results.as_mut_ptr();
+        unsafe {
+            group.bench_function(format!("n_values: {}", n_values), |b| {
+                b.iter(|| {
+                    eval(
+                        party_id,
+                        xs_pointer,
+                        keys_pointer,
+                        results_pointer,
+                        n_values,
+                        n_threads,
+                        op_id,
+                    )
+                })
+            });
+        }
     }
-    let party_id: usize = 0;
-    let xs: Vec<u8> = Vec::with_capacity(3105);
-    let xs_pointer: *const u8 = xs.as_ptr();
-    let keys_pointer: *const u8 = keys_a_pointer as *const u8;
-    let mut results: Vec<i64> = Vec::with_capacity(3105);
-    let results_pointer: *mut i64 = results.as_mut_ptr();
-    unsafe {
-        c.bench_function("Eq batch eval", |b| {
-            b.iter(|| {
-                eval(
-                    party_id,
-                    xs_pointer,
-                    keys_pointer,
-                    results_pointer,
-                    n_values,
-                    n_threads,
-                    op_id,
-                )
-            })
-        });
+    for &n_threads in [1, 4, 6].iter() {
+        let key_length: usize = 621;
+        let n_values: usize = 100;
+        let op_id: usize = 0;
+        let mut keys_a: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let mut keys_b: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let keys_a_pointer: *mut u8 = keys_a.as_mut_ptr();
+        let keys_b_pointer: *mut u8 = keys_b.as_mut_ptr();
+        unsafe {
+            keygen(keys_a_pointer, keys_b_pointer, n_values, n_threads, op_id);
+        }
+        let party_id: usize = 0;
+        let xs: Vec<u8> = Vec::with_capacity(key_length * n_values);
+        let xs_pointer: *const u8 = xs.as_ptr();
+        let keys_pointer: *const u8 = keys_a_pointer as *const u8;
+        let mut results: Vec<i64> = Vec::with_capacity(key_length * n_values);
+        let results_pointer: *mut i64 = results.as_mut_ptr();
+        unsafe {
+            group.bench_function(format!("n_threads: {}", n_threads), |b| {
+                b.iter(|| {
+                    eval(
+                        party_id,
+                        xs_pointer,
+                        keys_pointer,
+                        results_pointer,
+                        n_values,
+                        n_threads,
+                        op_id,
+                    )
+                })
+            });
+        }
     }
 }
 
