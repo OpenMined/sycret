5e5e1cb77696d1c99608047e0a44b81677c3cacd
Arturo Marquez <arturomf94@gmail.com>
2021-09-27 11:12:40 -0500

    Update Maturin version

--- sycret/sycret/__init__.py
+++ sycret/sycret/__init__.py
@@ -3,5 +3,5 @@ __all__ = ["lib", "ffi"]
 import os
 from .ffi import ffi
 
-lib = ffi.dlopen(os.path.join(os.path.dirname(__file__), 'native.so'), 4098)
+lib = ffi.dlopen(os.path.join(os.path.dirname(__file__), 'native.so'))
 del os
